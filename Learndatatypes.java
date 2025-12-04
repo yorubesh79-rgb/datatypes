9c3774e7e9e34f3f999588fff8a2550308e3ef4c
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-04 14:46:29 +0530

    Add files via upload
    second program

---
+++ Learndatatypes.java
@@ -0,0 +1,16 @@
+package learnautomation1;
+
+public class Learndatatypes {
+
+	public static void main(String[] args) {
+		String name="Rubesh";
+		int age=19;
+		char initial='V';
+	    System.out.println("Enter the name:"+name);
+		System.out.println("Enter the age:"+age);
+		System.out.println("Enter the initial:"+initial);
+		// TODO Auto-generated method stub
+
+	}
+
+}
