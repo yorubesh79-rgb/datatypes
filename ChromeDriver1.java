38c49b9d286e4f61b7497d3f342f9b3cce2654c4
yorubesh79-rgb <yorubesh79@gmail.com>
2025-12-05 14:42:10 +0530

    Add files via upload
    chrome driver

---
+++ ChromeDriver1.java
@@ -0,0 +1,13 @@
+package learnautomation1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeDriver1 {
+
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		// TODO Auto-generated method stub
+
+	}
+
+}
